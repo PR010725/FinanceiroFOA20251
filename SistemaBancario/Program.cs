1e775a966b84a5d562912b082a8256d3bb85be37
Joao-204 <joao2012vr@gmail.com>
2025-05-07 21:00:28 -0300

    herança

--- SistemaBancario/Program.cs
+++ SistemaBancario/Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using SistemaBancario.Model;
+
+Cliente cliente1 = new Cliente("Maria", "123456789000");
+Conta conta1 = new Conta(cliente1);
+
+Console.WriteLine($"A conta de número {conta1.Numero} pertence ao cliente {conta1.Titular.Nome}");
+
+ContaEspecial conta 2 = new ContaEspecial(1000, 500);
+Conta conta3 = new ContaEspecial(2000, 1000);
+
+conta3.Depositar(500);
+conta3.Sacar(1000);
